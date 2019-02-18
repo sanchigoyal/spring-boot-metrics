bf05143629aca6e5399e0274890a9b0cb13ed71e
sanchigoyal <sanchi_goyal@yahoo.in>
2019-02-17 23:47:54 -0800

    first commit

---
+++ src/main/java/com/sanchi/java/springbootmetrics/SpringBootMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.sanchi.java.springbootmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanchi/java/springbootmetrics/SpringBootMetricsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sanchi.java.springbootmetrics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMetricsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
